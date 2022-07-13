759a8f4eea209bbe0463bcf2c895aa10208e9471
Guillaume Rozier <guillaume.rozier@telecomnancy.net>
2022-07-13 10:27:28 +0200

    Adding data

--- src/script.py
+++ src/script.py
@@ -6,12 +6,13 @@ df_world = df[df["location"] == "World"]
 df = df[df["location"] != "World"]
 
 df_date_max = df[["location", "date", "total_confirmed_by_confirmation"]].groupby(["location"]).last().reset_index().sort_values(by="total_confirmed_by_confirmation", ascending=False)
-df_france = df[["location", "date", "total_confirmed_by_confirmation", "7day_confirmed_by_confirmation"]][df["location"] == "France"].reset_index()
+df_france = df[["location", "date", "total_confirmed_by_confirmation", "7day_confirmed_by_confirmation", "daily_confirmed_by_confirmation"]][df["location"] == "France"].reset_index()
 
 json_string = {}
 json_string["dates"] = df_france.date.tolist()
 json_string["total_confirmed_by_entry"] = df_france["total_confirmed_by_confirmation"].fillna(0).tolist()
 json_string["7day_confirmed_by_confirmation"] = df_france["7day_confirmed_by_confirmation"].fillna(0).tolist()
+json_string["daily_confirmed_by_confirmation"] = df_france["daily_confirmed_by_confirmation"].fillna(0).tolist()
 
 with open('data/monkeypox-france.json', 'w') as outfile:
     json.dump(json_string, outfile)
