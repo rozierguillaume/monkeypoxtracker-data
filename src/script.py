6344aaba2753cdf7b5b62a5e994ea0cf4c95f1b2
Guillaume Rozier <guillaume.rozier@telecomnancy.net>
2022-08-02 11:30:52 +0200

    fix column names

--- src/script.py
+++ src/script.py
@@ -5,14 +5,14 @@ df = pd.read_csv("https://raw.githubusercontent.com/owid/notebooks/main/EdouardM
 df_world = df[df["location"] == "World"]
 df = df[df["location"] != "World"]
 
-df_date_max = df[["location", "date", "total_confirmed_by_confirmation"]].groupby(["location"]).last().reset_index().sort_values(by="total_confirmed_by_confirmation", ascending=False)
-df_france = df[["location", "date", "total_confirmed_by_confirmation", "7day_confirmed_by_confirmation", "daily_confirmed_by_confirmation"]][df["location"] == "France"].reset_index()
+df_date_max = df[["location", "date", "total_cases"]].groupby(["location"]).last().reset_index().sort_values(by="total_cases", ascending=False)
+df_france = df[["location", "date", "total_cases", "new_cases_smoothed", "new_cases"]][df["location"] == "France"].reset_index()
 
 json_string = {}
 json_string["dates"] = df_france.date.tolist()
-json_string["total_confirmed_by_entry"] = df_france["total_confirmed_by_confirmation"].fillna(0).tolist()
-json_string["7day_confirmed_by_confirmation"] = df_france["7day_confirmed_by_confirmation"].fillna(0).tolist()
-json_string["daily_confirmed_by_confirmation"] = df_france["daily_confirmed_by_confirmation"].fillna(0).tolist()
+json_string["total_confirmed_by_entry"] = df_france["total_cases"].fillna(0).tolist()
+json_string["new_cases_smoothed"] = df_france["new_cases_smoothed"].fillna(0).tolist()
+json_string["new_cases"] = df_france["new_cases"].fillna(0).tolist()
 
 with open('data/monkeypox-france.json', 'w') as outfile:
     json.dump(json_string, outfile)
@@ -20,8 +20,8 @@ with open('data/monkeypox-france.json', 'w') as outfile:
 
 json_string = {}
 json_string["dates"] = df_world.date.tolist()
-json_string["total_confirmed_by_entry"] = df_world["total_confirmed_by_confirmation"].fillna(0).tolist()
-json_string["7day_confirmed_by_confirmation"] = df_world["7day_confirmed_by_confirmation"].fillna(0).tolist()
+json_string["total_confirmed_by_entry"] = df_world["total_cases"].fillna(0).tolist()
+json_string["new_cases_smoothed"] = df_world["new_cases_smoothed"].fillna(0).tolist()
 
 with open('data/monkeypox-world-only.json', 'w') as outfile:
     json.dump(json_string, outfile)
@@ -29,7 +29,7 @@ with open('data/monkeypox-world-only.json', 'w') as outfile:
 
 json_string = {}
 json_string["locations"] = df_date_max.location.tolist()
-json_string["total_confirmed_by_entry"] = df_date_max["total_confirmed_by_confirmation"].fillna(0).tolist()
+json_string["total_confirmed_by_entry"] = df_date_max["total_cases"].fillna(0).tolist()
 
 with open('data/monkeypox-world-countries-lastdate.json', 'w') as outfile:
     json.dump(json_string, outfile)
